3faf3d8ef5f040afccf6d8bf4ea7023ecd23d149
marg0sav <148487677+marg0sav@users.noreply.github.com>
2025-05-04 20:36:16 +0300

    Update variables.tf. Изменен синтаксис для хранения переменных

--- terraform-yandex/variables.tf
+++ terraform-yandex/variables.tf
@@ -1,6 +1,29 @@
-variable "yandex_token"        { type = string description = "YC OAuth-токен" }
-variable "cloud_id"            { type = string description = "ID облака" }
-variable "folder_id"           { type = string description = "ID каталога" }
-variable "zone"                { type = string description = "Зона" }
-variable "image_id"            { type = string description = "ID образа в Marketplace" }
-variable "ssh_public_key_path" { type = string description = "Путь к публичному SSH-ключу" }
+variable "yandex_token" {
+  type        = string
+  description = "YC OAuth-токен"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "ID облака"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "ID каталога"
+}
+
+variable "zone" {
+  type        = string
+  description = "Зона, например ru-central1-a"
+}
+
+variable "image_id" {
+  type        = string
+  description = "ID образа в Marketplace"
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  description = "Путь до публичного SSH-ключа"
+}
