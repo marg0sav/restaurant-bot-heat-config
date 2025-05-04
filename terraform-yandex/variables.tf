d98b19fb77d67a98606fc80073c1f227273c6df8
marg0sav <148487677+marg0sav@users.noreply.github.com>
2025-05-04 20:18:40 +0300

    Create variables.tf

---
+++ terraform-yandex/variables.tf
@@ -0,0 +1,6 @@
+variable "yandex_token"        { type = string description = "YC OAuth-токен" }
+variable "cloud_id"            { type = string description = "ID облака" }
+variable "folder_id"           { type = string description = "ID каталога" }
+variable "zone"                { type = string description = "Зона" }
+variable "image_id"            { type = string description = "ID образа в Marketplace" }
+variable "ssh_public_key_path" { type = string description = "Путь к публичному SSH-ключу" }
